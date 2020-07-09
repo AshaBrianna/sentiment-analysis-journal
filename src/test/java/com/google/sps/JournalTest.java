625a729799b031a722ba0021ef8745ee89b7c8b4
Chidera Okeke <merylokeke46@gmail.com>
2020-07-09 13:39:37 -0700

    comment out JournaLTest

--- src/test/java/com/google/sps/JournalTest.java
+++ src/test/java/com/google/sps/JournalTest.java
@@ -1,40 +1,40 @@
-package com.google.sps;
+// package com.google.sps;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import java.io.IOException;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import org.junit.Assert;
+// import org.junit.Before;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.junit.runners.JUnit4;
+// import java.io.IOException;
 
-@RunWith(JUnit4.class)
-public final class JournalTest {
-    private static final String positiveText = "I am happy and great!";
-    private static final String negativeText = "I am feeling sad and depressed"; 
+// @RunWith(JUnit4.class)
+// public final class JournalTest {
+//     private static final String positiveText = "I am happy and great!";
+//     private static final String negativeText = "I am feeling sad and depressed"; 
 
-    private Journal sentiment;
+//     private Journal sentiment;
 
-    @Before
-    public void setUp() {
-        sentiment = new Journal();
-    }
+//     @Before
+//     public void setUp() {
+//         sentiment = new Journal();
+//     }
 
-    // @Test
-    // public void theNegativeTest() throws IOException {
-    //     float score = sentiment.sentimentAnalysis(negativeText);
-    //     boolean actual = score < 0.0f;
+//     @Test
+//     public void theNegativeTest() throws IOException {
+//         float score = sentiment.sentimentAnalysis(negativeText);
+//         boolean actual = score < 0.0f;
 
-    //     Assert.assertEquals(true, actual);
-    // }
+//         Assert.assertEquals(true, actual);
+//     }
 
-    // @Test
-    // public void thePositiveTest() throws IOException {
-    //     float score = sentiment.sentimentAnalysis(positiveText);
-    //     boolean actual = score >= 0.0f;
+//     @Test
+//     public void thePositiveTest() throws IOException {
+//         float score = sentiment.sentimentAnalysis(positiveText);
+//         boolean actual = score >= 0.0f;
 
-    //     Assert.assertEquals(true, actual);
-    // }
+//         Assert.assertEquals(true, actual);
+//     }
 
-}
+// } 
