6dd9654f970f01e0476aff0b798e229ee4d5721d
Brock Pytlik <bpytlik@users.noreply.github.com>
2020-07-10 19:04:01 -0700

    check out produced file

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -2,8 +2,10 @@
 
 # Decrypt the file
 mkdir $HOME/secrets
-echo ${CREDENTIAL_PASSPHRASE:0:3}
+echo "HELLO"
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CREDENTIAL_PASSPHRASE" \
---output $HOME/secrets/spsTeam26.json spsTeam26.json.gpg
+    --output $HOME/secrets/spsTeam26.json spsTeam26.json.gpg
+ls $HOME/secrets/spsTeam26.json
+head -n 2 $HOME/secrets/spsTeam26.json
