0b15ccf3ad98a3cadbc9950bae0c31f273801fc8
Asha Johnson <51245624+AshaBrianna@users.noreply.github.com>
2020-07-19 09:08:13 -0700

    Update book.js
    update js functions so each submitted entry displays on a page

--- src/main/webapp/book.js
+++ src/main/webapp/book.js
@@ -1,15 +1,16 @@
-function loadEntries() {
-  fetch('/journal').then(response => response.json()).then((entries) => { // Fetch = RPC request to server and waits then gets response
-    const entriesListElement = document.getElementById('entry-container'); //entry-container is a 'div'
-    entries.forEach((entry) => {
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = entry.message;
-        // const theEntryElement = document.createElement('div.bk-content');
-        // theEntryElement.innerHTML = paragraph;
-        entriesListElement.appendChild(paragraph);
-
+function loadEntries(callback) {
+    fetch('/journal').then(response => response.json()).then((entries) => { // Fetch = RPC request to server and waits then gets response
+        const entriesListElement = document.getElementById('entry-container'); //entry-container is a 'div'
+        const referenceNode = document.getElementById("reference");
+        entries.forEach((entry) => {
+            thePage = document.createElement('div');
+            thePage.className += 'bk-content';
+            thePage.innerHTML = entry.message;
+            entriesListElement.insertBefore(thePage, referenceNode);
+            console.log(entry.score)
+        })
+        callback();
     });
-  });
 }
 
 var Books = (function() {
@@ -17,9 +18,9 @@ var Books = (function() {
         var $book = $( '#bk-list > li > div.book' ),
 		$parent = $book.parent(),
 		$page = $book.children( 'div.bk-page' ),
-		$bookview = $parent.find( 'button.bk-bookview' ),
-		$content = $page.children( 'div.bk-content' ), current = 0;
-		$parent.find( 'button.bk-bookback' ).on( 'click', function() {				
+		$bookview = $parent.find( 'button.bk-bookview' );
+		var $content = $page.children( 'div.bk-content' ), current = 0;
+		$parent.find( 'button.bk-bookback' ).on( 'click', function() {	
 		    $bookview.removeClass( 'bk-active' );
 			if( $book.data( 'flip' ) ) {	
 				$book.data( { opened : false, flip : false } ).removeClass( 'bk-viewback' ).addClass( 'bk-default' );
@@ -46,17 +47,17 @@ var Books = (function() {
 			var $navPrev = $( '<span class="bk-page-prev">&lt;</span>' ),
 			$navNext = $( '<span class="bk-page-next">&gt;</span>' );	
 			$page.append( $( '<nav></nav>' ).append( $navPrev, $navNext ) );
-            $navPrev.on( 'click', function() {
+            $navPrev.on( 'click', function() { 
 				if( current > 0 ) {
 					--current;
-					$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current' );
+					$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current');
 				}
 				return false;
 			} );
 			$navNext.on( 'click', function() {
 				if( current < $content.length - 1 ) {
 					++current;
-					$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current' );
+					$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current');
 				}
 				return false;
 			} );
