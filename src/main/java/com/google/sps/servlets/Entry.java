eac6042fac31531fec7cbce93755b71e5632c4e9
Chidera Okeke <merylokeke46@gmail.com>
2020-07-19 21:30:03 -0500

    Minor changes to files to demonstrate working authentication

--- src/main/java/com/google/sps/servlets/Entry.java
+++ src/main/java/com/google/sps/servlets/Entry.java
@@ -5,10 +5,10 @@ public final class Entry {
   private final long id;
   private final String message;
   private final long timestamp;
-  private final float score;
+  private final double score;
   //TODO: consider noting the user too
 
-  public Entry(long id, String message, long timestamp, float score) {
+  public Entry(long id, String message, long timestamp, double score) {
     this.id = id;
     this.message = message;
     this.timestamp = timestamp;
--- src/main/java/com/google/sps/servlets/JournalEntryServlet.java
+++ src/main/java/com/google/sps/servlets/JournalEntryServlet.java
@@ -3,13 +3,14 @@ package com.google.sps.servlets;
 import com.google.gson.Gson;
 import com.google.sps.servlets.Entry;
 import com.google.sps.Journal;
-import java.io.IOException;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import java.io.IOException;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -24,9 +25,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/journal")
 public final class JournalEntryServlet extends HttpServlet {
+    UserService userService = UserServiceFactory.getUserService();
+    String nickname = getUserNickname(userService.getCurrentUser().getUserId());
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Query query = new Query("Entry").addSort("timestamp", SortDirection.DESCENDING);
+        Query query = new Query(nickname).addSort("timestamp", SortDirection.DESCENDING);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);
         List<Entry> entries = new ArrayList<>();
@@ -34,29 +38,60 @@ public final class JournalEntryServlet extends HttpServlet {
             long id = entity.getKey().getId();
             String message = (String) entity.getProperty("message");
             long timestamp = (long) entity.getProperty("timestamp");
-            float score = (float) entity.getProperty("score");
+            double score = (double) entity.getProperty("score");
             Entry entryToAdd = new Entry(id, message, timestamp, score);
             entries.add(entryToAdd);
         }
         Gson gson = new Gson();
         response.setContentType("application/json;");
         response.getWriter().println(gson.toJson(entries));
+        
     }
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Journal sentiment = new Journal();
+
+        if (userService.isUserLoggedIn()) {
+            Journal sentiment = new Journal();
+
+            if (nickname == null) {
+                response.sendRedirect("/nickname");
+                return;
+            }
         
-        // Get the input from the form.
-        String message = request.getParameter("entry");
-        long timestamp = System.currentTimeMillis();
-        float score = sentiment.sentimentAnalysis(message);
-        System.out.println(score);
-        Entity entryEntity = new Entity("Entry");
-        entryEntity.setProperty("message", message);
-        entryEntity.setProperty("timestamp", timestamp);
-        entryEntity.setProperty("score", score);
+            // Get the input from the form.
+            String message = request.getParameter("message");
+            if (nickname == null) {
+            response.sendRedirect("/nickname");
+            return;
+            }
+            long timestamp = System.currentTimeMillis();
+            double score = sentiment.sentimentAnalysis(message);
+
+            Entity entryEntity = new Entity(nickname);
+            entryEntity.setProperty("message", message);
+            entryEntity.setProperty("timestamp", timestamp);
+            entryEntity.setProperty("score", score);
+            datastore.put(entryEntity, userinfo.getKey());
+            response.sendRedirect("home.html"); 
+        }
+        else{
+            response.sendRedirect("index.html");
+        }
+        
+    }
+
+    private String getUserNickname(String id) {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(entryEntity);
-        response.sendRedirect("/index.html"); //do we want to redirect home after entry is submitted?
+        Query query =
+            new Query("UserInfo")
+                .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
+        PreparedQuery results = datastore.prepare(query);
+        Entity entity = results.asSingleEntity();
+        if (entity == null) {
+        return "";
+        }
+        String nickname = (String) entity.getProperty("nickname");
+        return nickname;
     }
 }
